9e6679abf2e2e78b3a5905b31f1a19c6b523c9ab
Anton Bredl <antonbr13@gmail.com>
2016-03-14 13:52:14 -0600

    first set of prompts set up on attempt2

---
+++ problems.js
@@ -0,0 +1,154 @@
+// NUMBER ONE_____________
+   // Write a function that takes in a perfect square and returns the next perfect square
+   // if the the function is not passed a perfect square then return -1
+
+
+
+
+
+
+// ----------- NEXT PROBLEM -----------------
+
+// NUMBER TWO_____________
+
+   // Make a function that satisfies the below conditions
+
+   // var counter = getCounter();
+   // counter(); //2
+   // counter(); //4
+   // counter(); //6
+   // counter(); //8
+   // counter(); //10
+
+
+
+
+// ----------- NEXT PROBLEM -----------------
+
+// NUMBER THREE_____________
+
+
+
+   //given the below object, write a function that returns an object whose keys are the fields found above and whose values are objects containing all the values as keys and a count for how many times that value occurs
+
+   //e.g.
+   //{
+   //    kingdom: {Animalia: 6}
+   //    phylum: {Chordata: 6},
+   //    class: {Mammalia: 4, Reptilia: 1, Aves: 1}
+   //}
+
+
+
+   var animals = [
+       {
+           name: 'Dog',
+           kingdom: 'Animalia',
+           phylum: 'Chordata',
+           class: 'Mammalia',
+           order: 'Carnivora',
+           family: 'Canidae',
+           species: 'Canis lupus'
+       },
+       {
+           name: 'Cat',
+           kingdom: 'Animalia',
+           phylum: 'Chordata',
+           class: 'Mammalia',
+           order: 'Carnivora',
+           family: 'Felidae',
+           species: 'Felis catus'
+       },
+       {
+           name: 'Hippopotamus',
+           kingdom: 'Animalia',
+           phylum: 'Chordata',
+           class: 'Mammalia',
+           order: 'Artiodactyla',
+           family: 'Hippopotamidae',
+           species: 'Hippopotamus amphibius'
+       },
+       {
+           name: 'Playtpus',
+           kingdom: 'Animalia',
+           phylum: 'Chordata',
+           class: 'Mammalia',
+           order: 'Monotremata',
+           family: 'Ornithorhynchidae',
+           species: 'Ornithorhynchus anatinus'
+       },
+       {
+           name: 'Lizard',
+           kingdom: 'Animalia',
+           phylum: 'Chordata',
+           class: 'Reptilia',
+           order: 'Squamata',
+           family: 'Agamidae',
+           species: 'Pogona vitticeps'
+       },
+       {
+           name: 'Blackbird',
+           kingdom: 'Animalia',
+           phylum: 'Chordata',
+           class: 'Aves',
+           order: 'Passeriformes',
+           family: 'Turdidae',
+           species: 'Turdus merula'
+       }
+   ];
+
+
+
+// ----------- NEXT PROBLEM -----------------
+
+// NUMBER FOUR_____________
+
+   // Check if 'a' and 'b' in a string have three characters between them
+
+   var str = "THIS LAME borrowed my style.";
+   var str2 = "THIS LbME Aorrowed my style.";
+   var str3 = "This dude is a lame";
+
+
+
+// ----------- NEXT PROBLEM -----------------
+
+// NUMBER FIVE
+   // Reverse a string
+   // Take in a string as an argument of a function - have that function return the string in reverse order as a string
+
+
+
+// ----------- NEXT PROBLEM -----------------
+
+// NUMBER SIX_____________
+
+
+
+// ----------- NEXT PROBLEM -----------------
+
+// NUMBER SEVEN_____________
+
+// ----------- NEXT PROBLEM -----------------
+
+// NUMBER EIGHT_____________
+
+// ----------- NEXT PROBLEM -----------------
+
+// NUMBER NINE_____________
+
+// ----------- NEXT PROBLEM -----------------
+
+// NUMBER TEN_____________
+
+// ----------- NEXT PROBLEM -----------------
+
+// NUMBER 11_____________
+
+// ----------- NEXT PROBLEM -----------------
+
+// NUMBER 12_____________
+
+// ----------- NEXT PROBLEM -----------------
+
+// NUMBER 13_____________
