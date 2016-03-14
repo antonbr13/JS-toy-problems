1f7d4da3020d92019c260d0b90dbf12a33df9b88
Anton Bredl <antonbr13@gmail.com>
2016-03-14 13:06:59 -0600

    first problem prompt set up on master

---
+++ problems.js
@@ -0,0 +1,3 @@
+// NUMBER ONE_____________
+// Write a function that takes in a perfect square and returns the next perfect square
+// if the the function is not passed a perfect square then return -1
