0b9ffb2b865d625338981299281940cefb877120
Anton Bredl <antonbr13@gmail.com>
2016-03-15 12:54:16 -0600

    setting up bonfires on attempt2

---
+++ basic-bonfires.js
@@ -0,0 +1,99 @@
+// €€€€€
+// NUMBER ONE_____________
+   // Title Case a Sentence
+      // Return the provided string with the first letter of each word capitalized. Make sure the rest of the word is in lower case.
+
+      var string = "I'm a little tea pot";
+
+      function titleCase(str) {
+
+
+          // You do not need to check if i is larger than splitStr length, as your for does that for you
+          // Assign it back to the array
+
+
+
+        // Directly return the joined string
+
+      }
+
+      titleCase(string); //--> "I'm A Little Tea Pot"
+
+   // WALK-THROUGH:
+      // split the string into an array of single words so that we can iterate through those words as index values
+      // loop through each word(index) value and reassign the value to the same index value
+         // use charAt().toUpperCase() chained to capitalize the first letter of each word(index)
+         // add the rest of string with   '+ splitStr[i].substring(1)'
+
+         // .substring()
+            // strings are thought of like array indexes starting at zero
+            // .substring(2) applied to a var hello= 'hello anton';
+               // will produce 'llo anton'
+               // starts at and includes the index #2
+
+            // .substring(6,9) applied to a var hello= 'hello anton';
+                                       // index values: 01234567890
+               // will produce 'ant'
+               // starts at and includes index #6 --> goes up to but does not include index #9
+
+
+
+
+
+
+// NUMBER TWO_____________
+   // Return Largest Numbers in Arrays
+      // Return an array consisting of the largest number from each provided sub-array. For simplicity, the provided array will contain exactly 4 sub-arrays.
+
+      var pparentArray = [
+         [4, 5, 1, 3],
+         [13, 27, 18, 26],
+         [32, 35, 37, 39],
+         [1000, 1001, 857, 1]
+      ];
+
+      function largestOfFour(arr) {
+
+
+            // Iterate through parentArray
+            // Initial maximum
+
+            // Iterate subarrays
+            // Compare
+            // Update maximum
+
+
+            // Store the real maximum
+
+         }
+
+      largestOfFour(array);
+
+
+// NUMBER ONE_____________
+// Title Case a Sentence
+
+
+// NUMBER ONE_____________
+// Title Case a Sentence
+
+// NUMBER ONE_____________
+   // Title Case a Sentence
+
+// NUMBER ONE_____________
+// Title Case a Sentence
+// NUMBER ONE_____________
+// Title Case a Sentence
+// NUMBER ONE_____________
+// Title Case a Sentence
+// NUMBER ONE_____________
+// Title Case a Sentence
+// NUMBER ONE_____________
+// Title Case a Sentence
+// NUMBER ONE_____________
+// Title Case a Sentence
+// NUMBER ONE_____________
+// Title Case a Sentence
+
+// NUMBER ONE_____________
+// Title Case a Sentence
